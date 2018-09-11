acf990ec518e0f475db1beb37b17a3d68d55a7f2
Alex Silyuk <alex.silyuk@gmail.com>
2018-09-11 20:37:29 +0300

    Update unblockIPS.sh

--- Dos/unblockIPS.sh
+++ Dos/unblockIPS.sh
@@ -11,24 +11,20 @@ touch $updated_list
      exit 1
      fi;
 for i in $(cat $blocked_list); do
-        ip="${i#*:}"
-        echo $ip
-        time_stamp="${i%:*}"
-        echo $time_stamp
-        new_timestamp=$(($time_stamp + 600))
-        current_timestamp=$(date  +%s)
+	ip="${i#*:}"
+	time_stamp="${i%:*}"
+	new_timestamp=$(($time_stamp + 600))
+	current_timestamp=$(date  +%s)
 
-        if [ $current_timestamp -gt $new_timestamp ]; then
-                echo "remove $ip from iptables block"
-                sudo iptables -D INPUT -s $ip -j DROP 2>> /dev/null
-        else
-                echo "will expire in $((600- $(($current_timestamp-$time_stamp)))) seconds"
-                echo $i >> $updated_list
-        fi;
+	if [ $current_timestamp -gt $new_timestamp ]; then
+		echo "Remove $ip from iptables block"
+		sudo iptables -D INPUT -s $ip -j DROP 2>> /dev/null
+	else
+		echo "$ip block will expire in $((600- $(($current_timestamp-$time_stamp)))) seconds"
+		echo $i >> $updated_list
+	fi;
 done
 
-echo "rewriting blocked.txt"
 if [ -f $updated_list ]; then
-        mv $updated_list $blocked_list
+	mv $updated_list $blocked_list
 fi;
-
