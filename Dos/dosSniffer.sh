ca7c061f6a24c0414cb6479119e6a11506d697d2
Alex Silyuk <alex.silyuk@gmail.com>
2018-09-15 14:27:55 +0300

    Update dosSniffer.sh

--- Dos/dosSniffer.sh
+++ Dos/dosSniffer.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 outFile="out.txt"			#temp output file
 NETMASK="192.168.4.0/24"		#subnet mask
-detected_ips="detected_intruders.txt"	#file with ip's of DOS attackers 
 blocked_ips="blocked.txt"		#file with blocked ip's and blocking timestamp
 timeoutTime=60				#run check if 60 seconds to detect DOS attack
 packetsAmount=50			#amount of packet to be sure in DOS attack
+logFile="log.txt"
 
+if [ -f $logFile ]; then
+	rm $logFile
+fi;
 
 touch $blocked_ips
 #run forever loop
 while : ;
 do
-	#remove old file with intruders ip's
-	if [ -f $detected_ips ]; then
-		rm $detected_ips
-	fi;
 	
 	#run tcpdump with timer and determine syn and ack packets, catch 50 packets ( it's not ok to send 50 syn packets in 1 minute )
 	sudo timeout $timeoutTime tcpdump -nnq -i wlan0 -c $packetsAmount dst net $NETMASK and 'tcp[tcpflags] == tcp-syn or tcp[tcpflags] == tcp-ack' 2>> /dev/null  > $outFile
@@ -32,13 +31,13 @@ do
 		#if not in iptables rules to be DROPen, add rule nd write log
 		if [ $ip_already_blocked -eq 1 ];then
 			sudo iptables -A INPUT -s $attacker_ip -j DROP
-			echo "$(date +%D" "%H:%M:%S) : New attack from [ $attacker_ip ], create new DROP rule in iptables" >> log.txt
+			echo "$(date +%D" "%H:%M:%S) : New attack from [ $attacker_ip ], create new DROP rule in iptables" >> $logFile
 			
 			#add ip to files with blocked ip's for futer unblocking
 			echo "$(date +%s):$attacker_ip " >> $blocked_ips
 		else
 			#if ip already blocked, just write to log
-			echo "$(date +%D" "%H:%M:%S):Dos attack from [ $attacker_ip ] " >> log.txt
+			echo "$(date +%D" "%H:%M:%S) : Dos attack from [ $attacker_ip ] " >> $logFile
 		fi;
 		
 	fi;
