274d3e827891de1d53a3807db5b90203b91777e5
Alex Silyuk <alex.silyuk@gmail.com>
2018-09-15 15:36:00 +0000

    Update from router

--- EvilTwin/scanForTwins.sh
+++ EvilTwin/scanForTwins.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+
+while :; do
 #get amount of networks with name $SSID, that declares WIFI SSID
 num_of_nets=$(sudo iwlist wlan0 scan | grep "SID" | awk -F ':' '{print $2}' | grep $SSID | wc -l)
 if [ "$num_of_nets" != "1" ] ; then
-         echo "$num_of_nets networks with name \"$SSID\" were found near to you, maybe it's  Evil Twin?"
+	 echo "$num_of_nets networks with name \"$SSID\" were found near to you, maybe it's  Evil Twin?"
 else
-        echo "OK"       
+	echo "OK"	
 
 fi;
+
+
+sleep 30
+done;
